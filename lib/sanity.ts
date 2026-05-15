ee8657c1ab6ab70e1f6b1690bf70a4311e11e824
WadaHirono <donchan.hirono528@gmail.com>
2026-05-15 22:42:45 +0900

    env fix

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "tgv066pc",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01",
+  apiVersion: "2024-01-01",
   useCdn: false,
 });
